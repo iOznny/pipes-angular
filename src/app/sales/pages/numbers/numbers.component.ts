dc1412bb6f4db25450577a3ad85c29ad573ece22
iOznny <fo170014@gmail.com>
2021-03-16 12:00:38 -0600

    Avance

--- src/app/sales/pages/numbers/numbers.component.ts
+++ src/app/sales/pages/numbers/numbers.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html'
 })
 
-export class NumbersComponent implements OnInit {
+export class NumbersComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ventasNetas: number = 2567789.5567;
+  porcentaje: number = 0.48;
+  
 }
